6a8e3391340e2cb6187031f8faa06d91754ef723
chaithanya <chaithanya1812@gmail.com>
2024-12-23 14:10:17 +0530

    sample werf1

--- Day-1/main.tf
+++ Day-1/main.tf
@@ -41,7 +41,8 @@ resource "azurerm_linux_virtual_machine" "example" {
   network_interface_ids = [
     azurerm_network_interface.network-nic.id
   ]
-  source_image_id     = "LDOTrail-a8215d2e-c9a8-43ef-904d-c8b1ffb29cf7"
+  source_image_id     = "subscriptions/12f9be95-f674-4dc3-8c29-d915cc4e1f8e/resourceGroups/iteration-1/providers/Microsoft.Compute/images/devops-practice"
+  "
 
   os_disk {
     caching              = "ReadWrite"
