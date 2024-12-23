cc4e26826d07c502cb33f71a300fb42ebccea70f
chaithanya <chaithanya1812@gmail.com>
2024-12-23 13:42:16 +0530

    sample werf1

--- Day-1/main.tf
+++ Day-1/main.tf
@@ -5,6 +5,7 @@ provider "azurerm" {
   subscription_id = "12f9be95-f674-4dc3-8c29-d915cc4e1f8e"
 }
 
+# Persistence makes you Successful
 
 
 data "azurerm_resource_group" "example" {
@@ -13,4 +14,17 @@ data "azurerm_resource_group" "example" {
 
 output "main" {
   value = data.azurerm_resource_group.example
+}
+
+
+resource "azurerm_network_interface" "example" {
+  name                = "frontend-nic"
+  location            = data.azurerm_resource_group.example.location
+  resource_group_name = data.azurerm_resource_group.example.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = "/subscriptions/12f9be95-f674-4dc3-8c29-d915cc4e1f8e/resourceGroups/iteration-1/providers/Microsoft.Network/virtualNetworks/roboshop-net/subnets/subnet-1"
+    private_ip_address_allocation = "Dynamic"
+  }
 }
