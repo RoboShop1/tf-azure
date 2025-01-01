b2ab9025feb98b57b8f6b10c8abecdbbe39354a9
chaithanya <chaithanya1812@gmail.com>
2025-01-01 21:59:37 +0530

    sample werf1

--- Day-2/main.tf
+++ Day-2/main.tf
@@ -34,3 +34,28 @@ resource "azurerm_virtual_network" "example" {
     environment = "Roboshop-1"
   }
 }
+
+
+
+resource "azurerm_network_security_group" "r-net-sg" {
+  name                = "r-network"
+  location            = data.azurerm_resource_group.iteration-1.location
+  resource_group_name = data.azurerm_resource_group.iteration-1.name
+
+
+  security_rule {
+    name                       = "allow-ssh"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  tags = {
+    environment = "r-net-sg"
+  }
+}
