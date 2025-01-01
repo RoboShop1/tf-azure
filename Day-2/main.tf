cb30be1343bc37d3e4a24aa2cfa2cce9ef02294d
chaithanya <chaithanya1812@gmail.com>
2025-01-01 21:44:20 +0530

    sample werf1

--- Day-2/main.tf
+++ Day-2/main.tf
@@ -12,3 +12,25 @@ data "azurerm_resource_group" "iteration-1" {
 output "resource_group" {
   value = data.azurerm_resource_group.iteration-1
 }
+
+resource "azurerm_virtual_network" "example" {
+  name                = "Roboshop-net"
+  location            = data.azurerm_resource_group.iteration-1.location
+  resource_group_name = data.azurerm_resource_group.iteration-1.name
+  address_space       = ["10.0.0.0/16"]
+
+
+  subnet {
+    name             = "subnet1"
+    address_prefixes = ["10.0.1.0/24"]
+  }
+
+  subnet {
+    name             = "subnet2"
+    address_prefixes = ["10.0.2.0/24"]
+  }
+
+  tags = {
+    environment = "Roboshop-1"
+  }
+}
