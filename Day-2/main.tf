c87b97bfa8a6c0cee11ba12c48f16077e7e0ec64
chaithanya <chaithanya1812@gmail.com>
2025-01-01 21:41:26 +0530

    sample werf1

--- Day-2/main.tf
+++ Day-2/main.tf
@@ -5,6 +5,10 @@ provider "azurerm" {
   subscription_id = "12f9be95-f674-4dc3-8c29-d915cc4e1f8e"
 }
 
-data "azurerm_resource_group" "example" {
+data "azurerm_resource_group" "iteration-1" {
   name = "iteration-1"
+}
+
+output "resource_group" {
+  value = data.azurerm_resource_group.iteration-1
 }
