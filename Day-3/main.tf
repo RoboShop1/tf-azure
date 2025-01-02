43a8f58e4f6f4969704517ac6eaa0160f2618a05
chaithanya <chaithanya1812@gmail.com>
2025-01-02 12:51:09 +0530

    sample werf1

--- Day-3/main.tf
+++ Day-3/main.tf
@@ -2,6 +2,43 @@ module "vnet" {
   source = "./modules/vnet"
 }
 
-output "r_output" {
-  value = module.vnet
+output "vnet" {
+  value = module.vnet.virtual_network
 }
+
+# module "vm" {
+#
+#   depends_on = [module.vnet]
+#
+#   for_each                = var.vms
+#
+#   source                  = "./modules/vm"
+#   resource_group_location = module.vnet.resource_group.location
+#   component               = each.key
+#   resource_group_name     = module.vnet.resource_group.name
+#   subnet_id               = module.vnet.virtual_network.subnet.*.id[0]
+#   sg_ports                = each.key["sg_ports"]
+#
+#
+# }
+#
+#
+# variable "vms" {
+#   default = {
+#     web = {
+#       sg_ports = [22,80]
+#     }
+#
+#     app = {
+#       sg_ports = [22,8080]
+#     }
+#
+#     db = {
+#       sg_ports = [22,3306]
+#     }
+#
+#   }
+# }
+
+
+
--- Day-3/modules/vnet/outputs.tf
+++ Day-3/modules/vnet/outputs.tf
@@ -2,6 +2,9 @@ output "resource_group" {
   value = azurerm_resource_group.main
 }
 
+output "virtual_network" {
+  value  = azurerm_virtual_network.main
+}
 #
 # output "resource_group_name" {
 #   value = azurerm_resource_group.main
