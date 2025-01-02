b4100bcb0eb778e5b70bfef3a9c94272814f53dc
chaithanya <chaithanya1812@gmail.com>
2025-01-02 13:15:07 +0530

    sample werf1

--- Day-3/main.tf
+++ Day-3/main.tf
@@ -7,51 +7,51 @@ output "vnet" {
 }
 
 
-output "sample" {
-  value = <<EOF
-%{~ for name in module.vnet.virtual_network.subnet  ~}
-%{ if name["name"] == "app" }${name["id"]}%{ endif }
-%{~ endfor ~}
-EOF
-}
-
-# module "vm" {
-#
-#   depends_on = [module.vnet]
-#
-#   for_each                = var.vms
-#
-#   source                  = "./modules/vm"
-#   resource_group_location = module.vnet.resource_group.location
-#   component               = each.key
-#   resource_group_name     = module.vnet.resource_group.name
-#   subnet_id               = <<EOF
+# output "sample" {
+#   value = <<EOF
 # %{~ for name in module.vnet.virtual_network.subnet  ~}
-# %{ if name[each.value] == "web" }${name["id"]}%{ endif }
+# %{ if name["name"] == "app" }${name["id"]}%{ endif }
 # %{~ endfor ~}
 # EOF
-#   sg_ports                = each.key["sg_ports"]
-#
-#
 # }
-#
-#
-# variable "vms" {
-#   default = {
-#     web = {
-#       sg_ports = [22,80]
-#     }
-#
-#     app = {
-#       sg_ports = [22,8080]
-#     }
-#
-#     db = {
-#       sg_ports = [22,3306]
-#     }
-#
-#   }
-# }
+
+module "vm" {
+
+  depends_on = [module.vnet]
+
+  for_each                = var.vms
+
+  source                  = "./modules/vm"
+  resource_group_location = module.vnet.resource_group.location
+  component               = each.key
+  resource_group_name     = module.vnet.resource_group.name
+  subnet_id               = <<EOF
+%{~ for name in module.vnet.virtual_network.subnet  ~}
+%{ if name[each.key] == each.key }${name["id"]}%{ endif }
+%{~ endfor ~}
+EOF
+  sg_ports                = each.key["sg_ports"]
+
+
+}
+
+
+variable "vms" {
+  default = {
+    web = {
+      sg_ports = [22,80]
+    }
+
+    app = {
+      sg_ports = [22,8080]
+    }
+
+    db = {
+      sg_ports = [22,3306]
+    }
+
+  }
+}
 
 
 
