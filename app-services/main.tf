486b82145a7730cf9ac63fb53cd070e8892b1cbc
chaithanya <chaithanya1812@gmail.com>
2025-04-08 15:12:35 +0530

    ss

--- app-services/main.tf
+++ app-services/main.tf
@@ -9,3 +9,25 @@ resource "azurerm_service_plan" "example" {
   os_type             = "Linux"
   sku_name            = "B2"
 }
+
+resource "azurerm_linux_web_app" "example" {
+  name                = "nginx1"
+  resource_group_name = data.azurerm_resource_group.example.name
+  location            = data.azurerm_resource_group.example.location
+
+  service_plan_id     = azurerm_service_plan.example.id
+  public_network_access_enabled = true
+
+  app_settings = {
+    backend_ip = "localhost"
+  }
+
+  site_config {
+    application_stack {
+      docker_image_name = "chaitu1812/expense-frontend"
+      docker_registry_url = "https://index.docker.io"
+    }
+
+  }
+
+}
