d1ea861f32cc08e64cc787dce94a8ec1fe57ac8a
chaithanya <chaithanya1812@gmail.com>
2025-05-06 21:24:59 +0530

    ss

--- vnet-peering/modules/v-peering/main.tf
+++ vnet-peering/modules/v-peering/main.tf
@@ -9,3 +9,11 @@ resource "azurerm_virtual_network_peering" "example-1" {
   virtual_network_name      = "dev-network"
   remote_virtual_network_id = "/subscriptions/12f9be95-f674-4dc3-8c29-d915cc4e1f8e/resourceGroups/iteration-1/providers/Microsoft.Network/virtualNetworks/prod-network"
 }
+
+
+resource "azurerm_virtual_network_peering" "example-2" {
+  name                      = "devtoprod"
+  resource_group_name       = data.azurerm_resource_group.example.name
+  virtual_network_name      = "prod-network"
+  remote_virtual_network_id = "/subscriptions/12f9be95-f674-4dc3-8c29-d915cc4e1f8e/resourceGroups/iteration-1/providers/Microsoft.Network/virtualNetworks/dev-network"
+}
