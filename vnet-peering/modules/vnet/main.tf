11eb36eb620b4fe49edc931e7bb27c671fc1e376
chaithanya <chaithanya1812@gmail.com>
2025-05-06 20:50:36 +0530

    ss

--- vnet-peering/modules/vnet/main.tf
+++ vnet-peering/modules/vnet/main.tf
@@ -7,6 +7,32 @@ variable "net_range" {}
 variable "subnet1_range" {}
 variable "subnet2_range" {}
 
+
+
+resource "azurerm_network_security_group" "example" {
+  name                = "${var.network}-sg"
+  resource_group_name = data.azurerm_resource_group.example.name
+  location            = data.azurerm_resource_group.example.location
+
+  security_rule {
+    name                       = "allow_all_ssh"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  tags = {
+    environment = "${var.network}-sg"
+  }
+
+
+}
+
 resource "azurerm_virtual_network" "dev" {
   name                = var.network
   resource_group_name = data.azurerm_resource_group.example.name
@@ -16,6 +42,7 @@ resource "azurerm_virtual_network" "dev" {
   subnet {
     name             = "subnet1"
     address_prefixes = [var.subnet1_range]
+    security_group   = azurerm_network_security_group.example.id
   }
 
   subnet {
