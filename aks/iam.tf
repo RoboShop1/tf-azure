6522fef73713c15bf65d96ec3921f805fcf74256
chaithanya <chaithanya1812@gmail.com>
2025-05-27 08:50:40 +0530

    ss

--- aks/iam.tf
+++ aks/iam.tf
@@ -6,7 +6,7 @@ resource "azurerm_user_assigned_identity" "vault-identity" {
 }
 
 resource "azurerm_role_assignment" "vault-identity-role" {
-  principal_id = azurerm_user_assigned_identity.vault-identity.id
+  principal_id = azurerm_user_assigned_identity.vault-identity.principal_id
   scope        = "/subscriptions/12f9be95-f674-4dc3-8c29-d915cc4e1f8e/resourceGroups/iteration-1/providers/Microsoft.KeyVault/vaults/roboshop3"
   role_definition_name = "Key Vault Secrets User"
 }
