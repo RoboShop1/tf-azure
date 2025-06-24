9ca46c709cc34e6100011ebd7f9365aa7728ba18
chaithanya <chaithanya1812@gmail.com>
2025-06-24 22:21:56 +0530

    ss

--- One-1/output.tf
+++ One-1/output.tf
@@ -3,5 +3,5 @@ output "subnets" {
 }
 
 output "subnets1" {
-  value = { for i in azurerm_virtual_network.network.subnet: i.name => i.id }
+  value = { for i in azurerm_virtual_network.network.subnet: i.name => i.id if i.name == "subnet1" }
 }
