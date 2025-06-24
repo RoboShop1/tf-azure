7c3fef2b0aa131e4ec65c1e5e35e2da02c230735
chaithanya <chaithanya1812@gmail.com>
2025-06-24 22:33:32 +0530

    ss

--- One-1/main.tf
+++ One-1/main.tf
@@ -20,6 +20,21 @@ resource "azurerm_network_security_group" "main" {
 }
 
 
+resource "azurerm_network_security_rule" "add-sg-rule" {
+  name                        = "allow-http"
+  priority                    = 120
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "80"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = data.azurerm_resource_group.example.name
+  network_security_group_name = azurerm_network_security_group.main.name
+}
+
+
 
 
 resource "azurerm_virtual_network" "network" {
