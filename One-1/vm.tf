e334260994263dc162558a3ca180db4c9bb17d65
chaithanya <chaithanya1812@gmail.com>
2025-06-24 23:12:05 +0530

    ss

--- One-1/vm.tf
+++ One-1/vm.tf
@@ -42,7 +42,7 @@ resource "azurerm_linux_virtual_machine" "vm" {
 
   os_disk {
     caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
+    storage_account_type = "Standard_B2s"
   }
 
   source_image_reference {
