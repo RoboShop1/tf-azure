b214e7decd26e18e28758a06bf53470e5e6a17e1
chaithanya <chaithanya1812@gmail.com>
2025-06-24 23:01:18 +0530

    ss

--- One-1/vm.tf
+++ One-1/vm.tf
@@ -1,3 +1,16 @@
+resource "azurerm_public_ip" "main" {
+  name                = "one-ip"
+  location            = data.azurerm_resource_group.example.location
+  resource_group_name = data.azurerm_resource_group.example.name
+
+  allocation_method = "Static"
+
+  tags = {
+    component = "one-ip"
+  }
+
+
+}
 resource "azurerm_network_interface" "net" {
   name                = "example-nic"
   location            = data.azurerm_resource_group.example.location
@@ -8,6 +21,7 @@ resource "azurerm_network_interface" "net" {
     name                          = "internal"
     subnet_id                     = { for i in azurerm_virtual_network.network.subnet: i.name => i.id if i.name == "subnet1" }["subnet1"]
     private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.main.id
   }
 }
 
