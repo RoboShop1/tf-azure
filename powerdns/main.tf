9f891728305ca1a6224748e3a19ed854e3a35207
chaithanya <chaithanya1812@gmail.com>
2025-06-30 18:31:45 +0530

    ss

--- powerdns/main.tf
+++ powerdns/main.tf
@@ -1,3 +1,14 @@
+terraform {
+  required_providers {
+    powerdns = {
+      source = "pan-net/powerdns"
+      version = "1.5.0"
+    }
+  }
+}
+
+
+
 provider "powerdns" {
   api_key    = "supersecretapikey"
   server_url = "http://172.31.23.218:8081"
