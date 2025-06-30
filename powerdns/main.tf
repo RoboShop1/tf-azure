8089fe7353df3246b50ae40f4f0b14ffb1e345e4
chaithanya <chaithanya1812@gmail.com>
2025-06-30 18:58:13 +0530

    ss

--- powerdns/main.tf
+++ powerdns/main.tf
@@ -4,6 +4,10 @@ terraform {
       source  = "pan-net/powerdns"
       version = "1.5.0"
     }
+    null = {
+      source  = "hashicorp/null"
+      version = "3.2.2"
+    }
   }
 }
 
@@ -17,5 +21,23 @@ resource "powerdns_record" "www" {
   name    = "chaitu.spark.in."
   type    = "A"
   ttl     = 300
-  records = ["192.168.0.11"]
+  records = ["192.168.0.15"]
 }
+
+resource "null_resource" "run" {
+  depends_on = [powerdns_record.www]
+
+  triggers = {
+    # Triggers when any of these change:
+    record_id  = "${powerdns_record.www.zone}:${powerdns_record.www.name}:${powerdns_record.www.type}"
+    record_content = join(",", powerdns_record.www.records)  # Sensitive to IP changes
+    record_ttl = powerdns_record.www.ttl
+  }
+
+  provisioner "local-exec" {
+    command =<<EOT
+pdnsutil increase-serial spark.in
+pdns_control notify spark.in
+EOT
+  }
+  }
