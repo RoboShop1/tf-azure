87b2cfdcaafc0f76d9222858e1b20271c6a8d32a
chaithanya <chaithanya1812@gmail.com>
2025-06-30 18:48:35 +0530

    ss

--- powerdns/main.tf
+++ powerdns/main.tf
@@ -11,3 +11,11 @@ provider "powerdns" {
   api_key    = "supersecretapikey"
   server_url = "http://172.31.23.218:8081"
 }
+
+resource "powerdns_record" "www" {
+  zone    = "spark.in."
+  name    = "www.spark.in."
+  type    = "A"
+  ttl     = 300
+  records = ["192.168.0.11"]
+}
