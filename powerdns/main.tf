a233452351cbbddc866815df785b474ba3876747
chaithanya <chaithanya1812@gmail.com>
2025-06-30 18:46:54 +0530

    ss

--- powerdns/main.tf
+++ powerdns/main.tf
@@ -9,14 +9,5 @@ terraform {
 
 provider "powerdns" {
   api_key    = "supersecretapikey"
-  server_url = "https://172.31.23.218:8081/api/v1/servers"
-  insecure_https = true# Added /api/v1
-}
-
-resource "powerdns_record" "www" {
-  zone    = "spark.in."
-  name    = "www.spark.in."
-  type    = "A"
-  ttl     = 300
-  records = ["192.168.0.11"]
+  server_url = "http://172.31.23.218:8081"
 }
