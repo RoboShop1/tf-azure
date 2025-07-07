82c50e0a07f78a2d8cb34d950d300573ab6ab781
chaithanya <chaithanya1812@gmail.com>
2025-07-07 23:47:24 +0530

    ss

--- One-2/vnet.tf
+++ One-2/vnet.tf
@@ -28,6 +28,20 @@ resource "azurerm_network_security_group" "main" {
   #   destination_address_prefix = "*"
   # }
 
+
+
+  security_rule {
+    name                       = "allow-http"
+    priority                   = 120
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_application_security_group_ids = [azurerm_application_security_group.main.id]
+  }
+
   tags = {
     environment = "ssh-sg"
   }
