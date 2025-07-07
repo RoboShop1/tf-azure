847dc3c9c4f3cdd2adecd592bfe6656e3f1e1f3b
chaithanya <chaithanya1812@gmail.com>
2025-07-07 23:08:19 +0530

    ss

--- One-2/vnet.tf
+++ One-2/vnet.tf
@@ -15,25 +15,38 @@ resource "azurerm_network_security_group" "main" {
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
+
+  security_rule {
+    name                       = "allow-http"
+    priority                   = 120
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
   tags = {
     environment = "ssh-sg"
   }
 }
 
-
-resource "azurerm_network_security_rule" "add-sg-rule" {
-  name                        = "allow-http"
-  priority                    = 120
-  direction                   = "Inbound"
-  access                      = "Allow"
-  protocol                    = "Tcp"
-  source_port_range           = "*"
-  destination_port_range      = "80"
-  source_address_prefix       = "*"
-  destination_address_prefix  = "*"
-  resource_group_name         = data.azurerm_resource_group.example.name
-  network_security_group_name = azurerm_network_security_group.main.name
-}
+#
+# resource "azurerm_network_security_rule" "add-sg-rule" {
+#   name                        = "allow-http"
+#   priority                    = 120
+#   direction                   = "Inbound"
+#   access                      = "Allow"
+#   protocol                    = "Tcp"
+#   source_port_range           = "*"
+#   destination_port_range      = "80"
+#   source_address_prefix       = "*"
+#   destination_address_prefix  = "*"
+#   resource_group_name         = data.azurerm_resource_group.example.name
+#   network_security_group_name = azurerm_network_security_group.main.name
+# }
 
 
 
