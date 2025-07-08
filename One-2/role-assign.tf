c54f23de3df96520f6a81887970dbb24f824c4cc
chaithanya <chaithanya1812@gmail.com>
2025-07-08 23:55:10 +0530

    ss

--- One-2/role-assign.tf
+++ One-2/role-assign.tf
@@ -14,11 +14,25 @@ resource "azurerm_user_assigned_identity" "example" {
   resource_group_name = data.azurerm_resource_group.example.name
 }
 
-resource "azurerm_role_assignment" "role-assignment" {
-  name                 = "sample-assignment"
-  scope                = data.azurerm_key_vault.keyvault.id
-  role_definition_name = "Key Vault Secrets User"
-  principal_id         = azurerm_user_assigned_identity.example.client_id
+# resource "azurerm_role_assignment" "role-assignment" {
+#   name                 = "sample-assignment"
+#   scope                = data.azurerm_key_vault.keyvault.id
+#   role_definition_name = "Key Vault Secrets User"
+#   principal_id         = azurerm_user_assigned_identity.example.client_id
+# }
+
+
+
+resource "azuread_application" "example" {
+  display_name = "example"
 }
 
+resource "azuread_service_principal" "example" {
+  client_id                    = azuread_application.example.client_id
+}
 
+resource "azurerm_role_assignment" "example" {
+  scope                = data.azurerm_resource_group.example.id
+  role_definition_name = "Contributor"
+  principal_id         = azuread_service_principal.example.object_id
+}
