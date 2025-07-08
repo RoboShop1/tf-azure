3c1c9cffbd087114d31fab9a43da69ff5901f849
chaithanya <chaithanya1812@gmail.com>
2025-07-08 23:18:20 +0530

    ss

--- One-2/role-assign.tf
+++ One-2/role-assign.tf
@@ -14,4 +14,10 @@ resource "azurerm_user_assigned_identity" "example" {
   resource_group_name = data.azurerm_resource_group.example.name
 }
 
+resource "azurerm_role_assignment" "role-assignment" {
+  scope                = data.azurerm_key_vault.keyvault.id
+  role_definition_name = "Key Vault Secrets User"
+  principal_id         = azurerm_user_assigned_identity.example.client_id
+}
+
 
