b8b23fa057e555706937a473fdb9f27aaa68321f
chaithanya <chaithanya1812@gmail.com>
2025-07-11 17:29:09 +0530

    vault

--- vault-privateAccess/role.tf
+++ vault-privateAccess/role.tf
@@ -6,11 +6,11 @@ resource "azuread_service_principal" "sp" {
 
 }
 
-# resource "azurerm_role_assignment" "role" {
-#   scope                = azurerm_key_vault.newsecret.id
-#   role_definition_name = "Key Vault Secrets User"
-#   principal_id         = azuread_service_principal.sp.object_id
-# }
+resource "azurerm_role_assignment" "role" {
+  scope                = azurerm_key_vault.newsecret.id
+  role_definition_name = "Key Vault Secrets User"
+  principal_id         = azuread_service_principal.sp.object_id
+}
 
 resource "azuread_service_principal_password" "pass" {
   service_principal_id = azuread_service_principal.sp.id
